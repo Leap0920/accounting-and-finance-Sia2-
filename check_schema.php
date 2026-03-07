7847e889041167054b309a84119a489212e47d73
Leap0920 <carlobaclao123@gmail.com>
2026-03-07 15:22:09 +0800

    cleaning

--- check_schema.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'config/database.php';
-$result = $conn->query("DESCRIBE loan_applications");
-while ($row = $result->fetch_assoc()) {
-    print_r($row);
-}
-?>
--- tmp/truncate_file.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Truncate financial-reporting.php to first 565 lines
-$file = __DIR__ . '/../modules/financial-reporting.php';
-$lines = file($file);
-$clean = array_slice($lines, 0, 565);
-file_put_contents($file, implode('', $clean));
-echo "Done. File truncated to " . count($clean) . " lines.\n";
